d0ab25d7f0e21620157d423418f53a5ecdf359f4
Ali-tech13 <3looi3445@gmail.com>
2026-09-13 17:58:10 +0300

    Add file

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+Language = "Python"
+print(f"Language: {Language}")
+version = 3
+print(f"Version: {version}")
+pi = 3.14159
+print(f"Pi approx: {pi:.2f}")
+valid = 10 > 5
+print(f"Computation valid: {valid}")
