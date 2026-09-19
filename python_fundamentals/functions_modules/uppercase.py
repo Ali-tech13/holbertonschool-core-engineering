96a5eaeddf0a8fcb15953ba067cd65d955a31fbb
Ali-tech13 <3looi3445@gmail.com>
2026-09-19 21:20:37 +0300

    Edit file

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
+
 def uppercase(str):
     for char in str:
         if ord('a') <= ord(char) <= ord('z'):
-            print(chr(ord(char)- 32), end="")
-        else:
-            print(char, end="")
-        print()
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print()
